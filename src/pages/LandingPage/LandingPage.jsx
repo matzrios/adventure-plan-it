d3f1ae96bb2f83b231ba16bfaa5db93c7bce6d2c
Matthew Rios <riverground@outlook.com>
2024-04-12 14:13:36 -0400

    feat: :sparkles: added button and collage

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,11 +1,21 @@
 import "./LandingPage.scss";
+import FrameOne from "../../assets/images/Frame1.png";
 
 export default function LandingPage() {
   return (
-    <div className="logo--container">
-      <div className="logo">
-        <h1 className="logo__text">Adventure</h1>
-        <span className="logo__text-2">Plan-It</span>
+    <div className="landing-page">
+      <div className="logo--container">
+        <div className="logo">
+          <h1 className="logo__text">Adventure Plant-It</h1>
+        </div>
+      </div>
+
+      <div className="country-collage--container">
+        <img className="country-collage" src={FrameOne} alt="country collage" />
+      </div>
+
+      <div className="get-started-button--container">
+        <button className="get-started-button">Get Started Here</button>
       </div>
     </div>
   );
