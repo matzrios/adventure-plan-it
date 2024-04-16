bfc83444ed3990d5449b932b5ac096d64ad4034d
Matthew Rios <riverground@outlook.com>
2024-04-15 21:34:27 -0400

    feat: :sparkles: activities selection finished

--- src/pages/ActivitiesPage/ActivitiesPage.jsx
+++ src/pages/ActivitiesPage/ActivitiesPage.jsx
@@ -1,5 +1,11 @@
 import "./ActivitiesPage.scss";
 import FrameTwo from "../../assets/images/Frame2.png";
+import Skating from "../../assets/images/skating.png";
+import Kayaking from "../../assets/images/kayak.png";
+import Snowboarding from "../../assets/images/snowboarding.png";
+import Hiking from "../../assets/images/hiking.png";
+import Surfing from "../../assets/images/surfing.png";
+import Sailing from "../../assets/images/sailing.png";
 import { Link } from "react-router-dom";
 
 export default function LandingPage() {
@@ -9,8 +15,50 @@ export default function LandingPage() {
         <h2 className="activities-page-title">Activities</h2>
       </div>
 
+      <div className="activities-page-cards--container">
+        <div className="activities-page-cards">
+          <div className="activity-card">
+            <img className="activity-card--image" src={Skating} alt="skating" />
+            <p className="activity-card--title">Skating</p>
+          </div>
+
+          <div className="activity-card">
+            <img
+              className="activity-card--image"
+              src={Kayaking}
+              alt="kayaking"
+            />
+            <p className="activity-card--title">Kayaking</p>
+          </div>
+
+          <div className="activity-card">
+            <img
+              className="activity-card--image"
+              src={Snowboarding}
+              alt="snowboarding"
+            />
+            <p className="activity-card--title">Snowboarding</p>
+          </div>
+
+          <div className="activity-card">
+            <img className="activity-card--image" src={Hiking} alt="hiking" />
+            <p className="activity-card--title">Hiking</p>
+          </div>
+
+          <div className="activity-card">
+            <img className="activity-card--image" src={Surfing} alt="surfing" />
+            <p className="activity-card--title">Surfing</p>
+          </div>
+
+          <div className="activity-card">
+            <img className="activity-card--image" src={Sailing} alt="sailing" />
+            <p className="activity-card--title">Sailing</p>
+          </div>
+        </div>
+      </div>
+
       <div className="activities-country-collage--container">
-            <img className="activities-country-collage" src={FrameTwo} alt="" />
+        <img className="activities-country-collage" src={FrameTwo} alt="" />
       </div>
     </div>
   );
